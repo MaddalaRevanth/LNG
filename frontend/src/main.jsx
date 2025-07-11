598dcacfbd95b8fab9b1a058a526c10e6bcf04e5
Amruta Jayanti <142327526+AmrutaJayanti@users.noreply.github.com>
2025-07-11 21:40:52 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
 
-export const serverUrl="https://lng-3yh8.onrender.com"
+export const serverUrl="https://lng-1.onrender.com"
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <Provider store={store}>
