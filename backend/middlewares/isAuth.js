fb8dfb6b00a011fe805af941302ff6741bd813ff
Amruta Jayanti <142327526+AmrutaJayanti@users.noreply.github.com>
2025-07-11 22:04:59 +0530

    Update isAuth.js

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -1,20 +1,19 @@
-import jwt from "jsonwebtoken"
-const isAuth=async (req,res,next)=>{
-    try {
-        let token=req.cookies.token
-        if(!token){
-            return res.status(400).json({message:"token is not found"})
-        }
+import jwt from "jsonwebtoken";
 
-        let verifyToken=  jwt.verify(token,process.env.JWT_SECRET)
-        req.userId=verifyToken.userId
-        next()
-
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({message:`isauth error ${error}`})
+const isAuth = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      console.log("TOKEN MISSING");
+      return res.status(400).json({ message: "token is not found" });
     }
-}
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = verifyToken.userId;
+    next();
+  } catch (error) {
+    console.log("TOKEN VERIFY ERROR", error);
+    return res.status(500).json({ message: `isauth error ${error}` });
+  }
+};
 
-export default isAuth
+export default isAuth;
