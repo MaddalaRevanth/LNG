2f8c3af2df5c883a4c707fc449284f44f141d23d
Amruta Jayanti <142327526+AmrutaJayanti@users.noreply.github.com>
2025-07-12 07:35:31 +0530

    Update isAuth.js

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const isAuth = (req, res, next) => {
   try {
-    const token = req.cookies?.token;
+    const token = req.cookies.token;
     if (!token) {
       return res.status(401).json({ message: "Token missing" });
     }
@@ -11,10 +11,8 @@ const isAuth = (req, res, next) => {
     req.userId = decoded.userId;
     next();
   } catch (error) {
-    if (error.name === "TokenExpiredError") {
-      return res.status(401).json({ message: "Token expired" });
-    }
-    return res.status(403).json({ message: "Invalid token" });
+    console.log(error)
+        return res.status(500).json({message:`isauth error ${error}`})
   }
 };
 
